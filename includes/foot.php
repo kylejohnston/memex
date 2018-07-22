136d414b19a47e5ce775aa50069e59cfc0a54f33
Kyle Johnston <flow14@gmail.com>
2018-07-22 13:02:18 -0500

    Removed calls to blazy.js

--- includes/foot.php
+++ includes/foot.php
@@ -1,8 +1,1 @@
-    <script src="js/blazy.min.js"></script>
-    <script>
-        ;(function() {
-            // Initialize
-            var bLazy = new Blazy();
-        })();
-    </script>
 <!-- ANALYTICS _ COMING SOOOOOOON -->

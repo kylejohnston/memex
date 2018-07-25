86c844281c2d46c367876fd3575d367010a6c594
Kyle Johnston <flow14@gmail.com>
2018-07-25 18:32:57 -0500

    Updated description

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # memex
-A simple PHP application that reads all images from a directory displays them on a page. Images are displayed in a simple grid layout using CSS grid. Features a [https://codepen.io/gschier/pen/HCoqh](pure CSS lightbox) and lazy loading with [https://github.com/aFarkas/lazysizes](lazysize.js).
+A simple PHP application that reads all images from a directory and displays them on a page. Images are displayed in a simple grid layout using CSS grid. Features a [https://codepen.io/gschier/pen/HCoqh](pure CSS lightbox) and lazy loading with [https://github.com/aFarkas/lazysizes](lazysize.js).
 
 I'm using this as my personal [morgue file](https://en.wikipedia.org/wiki/Morgue_file) or flat file – kind of a self-hosted Pinterest that I use for design inspiration.
 
-I created a bare bones, starter version on this application here: [memex core](https://github.com/kylejohnston/memex-core)
+I created a bare bones, starter version of this application here: [memex core](https://github.com/kylejohnston/memex-core)
 
 The term memex comes from [Vanner Bush’s](https://en.wikipedia.org/wiki/Vannevar_Bush) 1945 The Atlantic Monthly article “[As We May Think](https://www.theatlantic.com/magazine/archive/1945/07/as-we-may-think/303881/).” The memex is described as “a device in which an individual stores all his books, records, and communications, and which is mechanized so that it may be consulted with exceeding speed and flexibility” and an “enlarged intimate supplement to memory.”
--- colophon.php
+++ colophon.php
@@ -4,6 +4,9 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+
+
   <title>colophon /// memex</title>
   <?php include('includes/head.php'); ?>
   <style>.yolo{background:yellow}</style>
@@ -17,7 +20,8 @@
 
   <article class="col-2">
     <div class="column content">
-      <h2>MEMEX is a simple PHP application that reads all images from a directory displays them on a page.</h2>
+      <h2>MEMEX is a simple PHP application that reads all images from a directory and displays them on a page.</h2>
+      <p>The goal of this project was to use a minimal amount of code to create a modern, robust gallery of visual inspiration.</p>
       <h2>How it works</h2>
       <p>On each page, a variable in a snippet of PHP defines the path to an image directory:
       </p>
@@ -26,13 +30,15 @@
         $dir = 'img/';<br>
         include('includes/renderer.php');
       </code>
-      <p><strong>renderer.php</strong> looks for any images in that directory, including JPGs, PNGs, and GIFs. For each image found, it renders in a DIV with a link to view the image in a <a href="https://codepen.io/gschier/pen/HCoqh">pure CSS lightbox</a>.</p>
+      <p><strong>renderer.php</strong> looks for any images in that directory, including JPGs, PNGs, and GIFs. For each image found, it renders in a DIV with a link to view the full-size image in a <a href="https://codepen.io/gschier/pen/HCoqh">pure CSS lightbox</a>.</p>
       <p>The images are displayed in a simple grid (using CSS Grid) and <a href="https://github.com/aFarkas/lazysizes">lazysize.js</a> is used to lazy load the images.</p>
+      <p>To help with performance, I create a small version of each image (≤ 600px W), which is displayed on the page. The full-size image is loaded only when it's clicked or tapped.</p>
       <p>A bare bones, starter kit version on this application is available on GitHub: <a href="https://github.com/kylejohnston/memex-core">memex core</a>.</p>
     </div>
     <div class="column">
       <div class="inner">
         <h3>Design notes</h3>
+        <p>The overall design is inspired by the principles of <a href="https://en.wikipedia.org/wiki/Brutalist_architecture">brutalism</a> and minimalism.</p>
         <p><strong>Font:</strong> monospace</p>
         <p><strong>Colors:</strong>
           <span class="hexFFFCFA">#FFFCFA</span>
@@ -44,7 +50,17 @@
         </p>
         <h3>A note on file naming</h3>
         <p>Whenever possible, files are named to give credit to (or at least hint at) the source and/or creator.</p>
-        <p>For example, a screenshot of a website: <code>someurl.com/project-name</code> becomes <code>someurl_com__project-name.jpg</code> </p>
+        <p>For example, a screenshot of a website: <code>someurl.com/project-name</code> becomes <code>someurl_com__project-name.jpg</code></p>
+        <h3>Tools used</h3>
+        <p>
+	        <a href="http://digitalocean.com">DigitalOcean</a> - web hosting<br>
+	        <a href="https://imageoptim.com/mac">ImageOptim</a> - image optimization<br>
+	        <a href="https://www.figma.com/">Figma</a> - vector graphics and logo design<br>
+	        <a href="https://www.adobe.com/products/photoshop.html">Photoshop</a> - image preparation<br>
+        	<a href="https://birme.net/blog/bulk-resize-images-with-sips-on-mac/">sips</a> - super-fast bulk image resizing<br>
+        	<a href="https://www.panic.com/transmit/">Transmit</a> - FTP client<br>
+	        <a href="http://sublimetext.com">Sublime Text</a> - coding<br>
+	    </p>
     </div>
   </article>
 

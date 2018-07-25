29b6d7ff12569cc57bfe59ec25a9e99599ed6819
Kyle Johnston <flow14@gmail.com>
2018-07-25 16:12:07 -0500

    Added analytics

--- includes/foot.php
+++ includes/foot.php
@@ -1,1 +1,13 @@
-<!-- ANALYTICS _ COMING SOOOOOOON -->
+<script type="text/javascript">
+var sc_project=11772915; 
+var sc_invisible=1; 
+var sc_security="abfe786d"; 
+</script>
+<script type="text/javascript"
+src="https://www.statcounter.com/counter/counter.js"
+async></script>
+<noscript><div class="statcounter"><a title="web counter"
+href="http://statcounter.com/" target="_blank"><img
+class="statcounter"
+src="//c.statcounter.com/11772915/0/abfe786d/1/" alt="web
+counter"></a></div></noscript>
--- includes/head.php
+++ includes/head.php
@@ -1,2 +1,11 @@
 <link rel="stylesheet" href="css/styles.css">
-<script src="js/lazysizes.min.js" type="text/javascript"></script>
+<script src="js/lazysizes.min.js"></script>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-92556-27"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-92556-27');
+</script>

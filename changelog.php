a50a314775138c368ccf03ebfe5752d7ee3ec4f3
Kyle Johnston <flow14@gmail.com>
2018-07-27 12:25:11 -0500

    Initial commit

---
+++ changelog.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>changelog /// memex</title>
+  <?php include('includes/head.php'); ?>
+</head>
+<body>
+  <!-- CONTENT GOES HERE -->
+  <header>
+    <?php include('includes/header-nav.php'); ?>
+  </header>
+  <div class="col-1 content"><h1><span>Changelog</span></h1></div>
+
+  <article class="col-2">
+    <div class="column content">
+      <p>2018-JUL-25 : Launch</p>
+    </div>
+    <div class="column shim"></div>
+  </article>
+
+  <footer>
+    <?php include('includes/footer.php'); ?>
+  </footer>
+  <?php include('includes/foot.php'); ?>
+</body>
+</html>

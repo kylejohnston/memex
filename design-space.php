ec6196b49a797473ed327ad3a20d07b4ca96ffab
Kyle Johnston <flow14@gmail.com>
2019-08-31 12:58:41 -0500

    Initial commit
    Formerly known as design-office

---
+++ design-space.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>design / space /// memex</title>
+  <?php include('includes/head.php'); ?>
+</head>
+<body>
+  <!-- CONTENT GOES HERE -->
+  <header>
+    <?php include('includes/header-nav.php'); ?>
+  </header>
+  <div class="content">
+    <h1><span>design &horbar;&horbar; space</span></h1>
+    <article class="col-3">
+      <?php
+        $www_root = 'img/design/space';
+        $dir = 'img/design/space';
+        include('includes/renderer.php');
+      ?>
+    </article>
+  </div>
+  <footer>
+    <?php include('includes/footer.php'); ?>
+  </footer>
+  <?php include('includes/foot.php'); ?>
+</body>
+</html>

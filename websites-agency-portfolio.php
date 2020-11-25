7badb589e6950957cb0738895cd7b7771f2fef74
Kyle Johnston <flow14@gmail.com>
2020-11-25 11:50:35 -0600

    New directory. Initial commit.

---
+++ websites-agency-portfolio.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>web design &horbar; agency &times; portfolio /// memex</title>
+  <?php include('includes/head.php'); ?>
+</head>
+<body>
+  <!-- CONTENT GOES HERE -->
+  <header>
+    <?php include('includes/header-nav.php'); ?>
+  </header>
+  <div class="content">
+    <h1><span>web design &horbar;&horbar; agency &times; portfolio</span></h1>
+    <article class="col-5">
+      <?php
+        $www_root = 'img/websites/agency-portfolio';
+        $dir = 'img/websites/agency-portfolio/preview';
+        include('includes/renderer.php');
+      ?>
+    </article>
+  </div>
+  <footer>
+    <?php include('includes/footer.php'); ?>
+  </footer>
+  <?php include('includes/foot.php'); ?>
+</body>
+</html>
---
+++ websites-ux-wireframes.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>web design &horbar; ux &times; wireframes /// memex</title>
+  <?php include('includes/head.php'); ?>
+</head>
+<body>
+  <!-- CONTENT GOES HERE -->
+  <header>
+    <?php include('includes/header-nav.php'); ?>
+  </header>
+  <div class="content">
+    <h1><span>web design &horbar;&horbar; ux &times; wireframes</span></h1>
+    <article class="col-5">
+      <?php
+        $www_root = 'img/websites/ux-wireframes';
+        $dir = 'img/websites/ux-wireframes/preview';
+        include('includes/renderer.php');
+      ?>
+    </article>
+  </div>
+  <footer>
+    <?php include('includes/footer.php'); ?>
+  </footer>
+  <?php include('includes/foot.php'); ?>
+</body>
+</html>

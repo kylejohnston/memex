2187f8ffca9dc8063132942669134c9ac5895525
Kyle Johnston <flow14@gmail.com>
2020-11-25 11:51:50 -0600

    Updated image paths

--- includes/renderer.php
+++ includes/renderer.php
@@ -14,10 +14,10 @@
         echo '
         <div class="item">
           <a href="#',$file,'">
-            <img class="lazyload" src="img/loader-x.svg" data-src="', $www_root, '/', $file,'" alt="', $file, '" width="auto">
+            <img class="lazyload" src="img/loader-x.svg" data-src="', $www_root, '/preview/', $file,'" alt="', $file, '" width="auto">
           </a>
         </div>
-        <a href="#_" class="lightbox" id="',$file,'"><img class="lazyload" src="img/loader-m.svg" data-src="', $www_root, '/_fullsize/', $file, '"></a>'
+        <a href="#_" class="lightbox" id="',$file,'"><img class="lazyload" src="img/loader-m.svg" data-src="', $www_root, '/fullsize/', $file, '"></a>'
         ;
         //break;
       }

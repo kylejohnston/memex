2ad8eba79ed7efb17ff9b3e533ff7e07625b328e
Kyle Johnston <flow14@gmail.com>
2023-01-15 16:49:07 -0600

    New page; initial commit

---
+++ architecture-space.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>art &times; illustration /// memex</title>
+  <?php include('includes/head.php'); ?>
+</head>
+<body>
+  <!-- CONTENT GOES HERE -->
+  <header>
+    <?php include('includes/header-nav.php'); ?>
+  </header>
+  <div class="content">
+    <!-- 001 -->
+    <h1><span>architecture &times; space</span></h1>
+    <article class="col-3">
+      <?php
+        $www_root = 'img/architecture-space';
+        $dir = 'img/architecture-space/preview';
+        include('includes/renderer.php');
+      ?>
+    </article>
+
+  </div>
+  <footer>
+    <?php include('includes/footer.php'); ?>
+  </footer>
+  <?php include('includes/foot.php'); ?>
+</body>
+</html>

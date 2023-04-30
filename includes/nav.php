f056385d1888ac7bf216fdf672e3bd467cd1b24a
Kyle Johnston <flow14@gmail.com>
2023-04-30 18:13:38 -0500

    Initial commit

---
+++ includes/nav.php
@@ -0,0 +1,59 @@
+<nav class="column">
+  <div class="menu">
+    <p><a href="#menu">
+      <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="14" height="14" style="margin:0;">
+        <circle cx="50" cy="50" r="50" />
+      </svg>
+      menu
+    </a></p>
+    <div href="/" class="overlay " id="menu" >
+      <ul class="column order--1">
+        <li><a href="architecture-space.php">architecture &times; space</a></li>
+        <li><a href="art-illustration.php">art &times; illustration</a></li>
+        <li><a href="color.php">color</a></li>
+        <li><a href="copywriting.php">copywriting</a></li>
+        <li><a href="design.php">design</a> <span class="dash"></span>
+        <ul>
+          <li><a href="design-apps.php">apps</a></li>
+          <li><a href="design-brand-logos.php">brand &times; logos</a></li>
+          <li><a href="design-business-cards.php">business cards</a></li>
+          <li><a href="design-data-visualization.php"><nobr>data visualization</nobr></a></li>
+          <li><a href="design-goods.php">goods</a></li>
+          <li><a href="design-patterns.php">patterns</a></li>
+          <li><a href="design-political.php">political</a></li>
+          <li><a href="design-process.php">process</a></li>
+          <li><a href="design-typography.php">typography</a></li>
+        </ul>
+        </li>
+        <li><a href="ephemera.php">ephemera</a></li>
+        <li><a href="photography.php">photography</a></li>
+        <li><a href="strategy.php">strategy</a></li>
+        <li><a href="quotes.php">quotes</a></li>
+        <li><a href="websites.php">web design</a> <span class="dash"></span>
+
+        <ul>
+          <li><a href="websites-elements.php">elements</a></li>
+          <li><a href="websites-agency-portfolio.php">portfolios</a></li>
+          <li><a href="websites-product.php">product</a></li>
+          <li><a href="websites-ux-wireframes.php">ux</a></li>
+        </ul>
+        </li>
+        <li><a href="/">home</a></li>
+        <li><a href="inbox.php">inbox</a></li>
+        <li class="small">
+          <ul>
+            <li>A <a href="https://www.flow14.com">flow14</a> project // <a href="colophon.php">Colophon</a> // <a href="changelog.php">Changelog</a></li>
+          </ul>
+        </li>
+      </ul>
+      <ul class="column order--2">
+        <li><a href="#">
+          <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="14" height="14" style="margin:0;">
+              <circle cx="50" cy="50" r="50" />
+          </svg>
+          close
+        </a></li>
+      </ul>
+    </div>
+  </div>
+</nav>

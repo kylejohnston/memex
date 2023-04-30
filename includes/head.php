0f8376f8b83122f9db7263d3059da566aff55b7c
Kyle Johnston <flow14@gmail.com>
2023-04-30 18:15:08 -0500

    Nav refresh…
    - Removed Google fonts

--- includes/head.php
+++ includes/head.php
@@ -1,18 +1,9 @@
-<link rel="stylesheet" href="css/styles.css?20230115_0002">
+<link rel="stylesheet" href="css/styles.css?2023_04_30_0001">
 <script src="js/lazysizes.min.js"></script>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-92556-27"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-92556-27');
-</script>
 <!--
-XX    XX  XXXXX  XX    XX  XXXXX  XX   XX
-XXX  XXX  XX     XXX  XXX  XX      XX XX
-XX XX XX  XXXX   XX XX XX  XXXX     XXX
-XX    XX  XX     XX    XX  XX      XX XX
-XX    XX  XXXXX  XX    XX  XXXXX  XX   XX
- -->
+XX     XX  XXXXXX  XX     XX  XXXXXX  XX   XX
+XXX   XXX  XX      XXX   XXX  XX       XX XX
+XX XXX XX  XXXX    XX XXX XX  XXXX      XXX
+XX  X  XX  XX      XX  X  XX  XX       XX XX
+XX     XX  XXXXXX  XX     XX  XXXXXX  XX   XX
+-->

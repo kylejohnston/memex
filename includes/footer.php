365c751ca8cd4753fe442a397cde501aeb9367e5
Kyle Johnston <flow14@gmail.com>
2023-04-30 19:31:28 -0500

    Fixed link spacing

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
 <?php include('includes/logo.php'); ?>
 <div class="info">
 	<p class="small">A <a href="https://www.flow14.com">flow14</a> project // <a href="colophon.php">Colophon</a> // <a href="changelog.php">Changelog</a></p>
-	<p class="small">
+	<p class="small" style="margin-top: .5em;">
     <a href="#top">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" width="10" height="10" style="position:relative; top:.08em;">
       <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />

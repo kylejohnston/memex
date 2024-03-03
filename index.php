64d8e136a85839e54e9f3257825aefe11c80a5b3
Kyle Johnston <flow14@gmail.com>
2024-03-03 12:57:12 -0600

    Updated links, last update date

--- index.php
+++ index.php
@@ -29,35 +29,37 @@
           <li><a href="art-illustration.php">art &times; illustration</a></li>
           <li><a href="color.php">color</a></li>
           <li><a href="copywriting.php">copywriting</a></li>
-          <li><a href="design.php">design</a> <span class="dash"></span>
+          <li><a href="design.php">design index</a> <span class="dash"></span>
             <ul>
-              <li><a href="design-apps.php">apps</a></li>
-              <li><a href="design-brand-logos.php">brand &times; logos</a></li>
-              <li><a href="design-business-cards.php">business cards</a></li>
-              <li><a href="design-data-visualization.php"><nobr>data visualization</nobr></a></li>
-              <li><a href="design-goods.php">goods</a></li>
-              <li><a href="design-patterns.php">patterns</a></li>
-              <li><a href="design-political.php">political</a></li>
-              <li><a href="design-process.php">process</a></li>
-              <li><a href="design-typography.php">typography</a></li>
+              <li><a href="d-apps.php">apps</a></li>
+              <li><a href="d-brand-logos.php">brand &times; logos</a></li>
+              <li><a href="d-business-cards.php">business cards</a></li>
+              <li><a href="d-data-visualization.php"><nobr>data visualization</nobr></a></li>
+              <li><a href="d-patterns.php">patterns</a></li>
+              <li><a href="d-political.php">political</a></li>
+              <li><a href="d-process.php">process</a></li>
+              <li><a href="d-typography.php">typography</a></li>
+              <li><a href="d-visual-design.php">visual design</a></li>
             </ul>
           </li>
           <li><a href="ephemera.php">ephemera</a></li>
+          <li><a href="goods.php">goods</a></li>
           <li><a href="photography.php">photography</a></li>
           <li><a href="strategy.php">strategy</a></li>
           <li><a href="quotes.php">quotes</a></li>
-          <li><a href="websites.php">web design</a> <span class="dash"></span>
+          <li><a href="websites.php">websites index</a> <span class="dash"></span>
             <ul>
-              <li><a href="websites-elements.php">elements</a></li>
-              <li><a href="websites-agency-portfolio.php">portfolios</a></li>
-              <li><a href="websites-product.php">product</a></li>
-              <li><a href="websites-ux-wireframes.php">ux</a></li>
+              <li><a href="w-elements.php">elements</a></li>
+              <li><a href="w-agency-portfolio.php">portfolios</a></li>
+              <li><a href="w-product.php">product</a></li>
+              <li><a href="w-ux-wireframes.php">ux</a></li>
+              <li><a href="w-websites.php">websites</a></li>
             </ul>
           </li>
-          <li><a href="inbox.php">inbox</a></li>
+          <li><a href="inbox.php">⁖ inbox</a></li>
         </ul>
       </div>
-      <p>&horbar;&horbar;&horbar;&horbar;<br><a href="changelog.php">Changelog</a> : Last update 2023-APR-25</p>
+      <p>&horbar;&horbar;&horbar;&horbar;<br><a href="changelog.php">Changelog</a> : Last update 2024-MAR-03</p>
     </div>
   </article>
   <?php include('includes/foot.php'); ?>

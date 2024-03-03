a36f916cb808bb2e05d1ff471fede0eb1d6c1c1f
Kyle Johnston <flow14@gmail.com>
2024-03-03 12:56:57 -0600

    Refactored this a bit…
    - Separated some of the logic to add the functionality to display the number of images in each directory

--- includes/renderer.php
+++ includes/renderer.php
@@ -1,26 +1,17 @@
 <?php
-  $file_display = array('jpg', 'jpeg', 'png', 'gif', 'webp');
-
-  if ( file_exists( $dir ) == false ) {
-    echo 'Directory \'', $dir, '\' not found!';
-  } else {
-    $dir_contents = scandir( $dir );
-    shuffle($dir_contents); // randomize the display order
-
-    foreach ($dir_contents as $file) {
-      $explode = explode('.', $file);
-      $file_type = strtolower(end($explode));
-      if ( ($file !== '.') && ($file !== '..') && (in_array( $file_type, $file_display)) ) {
-        echo '
-        <div class="item">
-          <a href="#',$file,'">
-            <img class="lazyload" src="img/loader-x.svg" data-src="', $www_root, '/preview/', $file,'" alt="', $file, '" width="auto">
-          </a>
-        </div>
-        <a href="#_" class="lightbox" id="',$file,'"><img class="lazyload" src="img/loader-m.svg" data-src="', $www_root, '/fullsize/', $file, '"></a>'
-        ;
-        //break;
-      }
-    }
+foreach ($dir_contents as $file) {
+  $explode = explode('.', $file);
+  $file_type = strtolower(end($explode));
+  if ( ($file !== '.') && ($file !== '..') && (in_array( $file_type, $file_display)) ) {
+    echo '
+    <div class="item">
+    <a href="#',$file,'">
+    <img class="lazyload" src="img/loader-x.svg" data-src="', $www_root, '/preview/', $file,'" alt="', $file, '" width="auto">
+    </a>
+    </div>
+    <a href="#_" class="lightbox" id="',$file,'"><img class="lazyload" src="img/loader-m.svg" data-src="', $www_root, '/fullsize/', $file, '"></a>'
+    ;
+    //break;
   }
+}
 ?>
